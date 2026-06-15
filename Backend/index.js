3a0b591b298a2ddfd4a7d3e477670d3c2bd06a0f
oyeshi13 <oyeshi1308@gmail.com>
2026-06-15 18:26:30 +0600

    can read multiple files

--- Backend/index.js
+++ Backend/index.js
@@ -1,56 +1,55 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import multer from 'multer'
-import {PDFParse} from 'pdf-parse'
-import fs from 'fs'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import { PDFParse } from "pdf-parse";
+import fs from "fs";
 
-
-const app=express()
-const PORT=5000
-const storage = multer.memoryStorage()
+const app = express();
+const PORT = 5000;
+const storage = multer.memoryStorage();
 
 const upload = multer({
-    storage
-})
+  storage,
+});
 
+app.use(express.json());
+app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("backend running");
+});
 
-app.use(express.json())
-app.use(cors())
+app.post("/chat", (req, res) => {
+  const msg = req.body.message;
+  res.json({
+    reply: msg,
+  });
+});
 
-app.get('/',(req,res)=>{
-    res.send("backend running")
-})
+app.post("/analyze-resume", upload.array("files"), async (req, res) => {
+  const files = req.files;
+  const prompt = req.body.prompt;
+  let allDocs = [];
+  try {
+    for (const file of files) {
+      const uint8 = new Uint8Array(file.buffer);
+      const parse = await new PDFParse(uint8);
 
-app.post('/chat',(req,res)=>{
-    const msg = req.body.message
-    res.json({
-        reply : msg
-    })
-})
-
-app.post("/analyze-resume",upload.array("files"),async (req,res)=>{
-    const files = req.files
-    const prompt = req.body.prompt
-    
-    try{
-        const uint8 = new Uint8Array(files[0].buffer)
-        const parse = await new PDFParse(uint8)
-
-        const result = await parse.getText()
-        res.json({
-        reply: result.text.slice(0,150)
-    })
-    }catch(err){
-        res.json({
-            reply: err.message
-        })
+      const result = await parse.getText();
+      allDocs.push(result.text.slice(0, 50));
     }
-    
 
-})
+    res.json({
+      reply: allDocs,
+    });
+  } catch (err) {
+    res.json({
+      reply: err.message,
+    });
+  }
+});
 
-app.listen(PORT,()=>{
-    console.log(`Backend running in http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Backend running in http://localhost:${PORT}`);
+});
